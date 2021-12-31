3b251a96af9c13d6a925dc2ced3f7ca04e958f2b
matt468779 <mengistusima@gmail.com>
2022-01-01 00:34:54 +0300

    minor fixes

--- gagarian_rest/settings.py
+++ gagarian_rest/settings.py
@@ -85,9 +85,9 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-CORS_ORIGIN_WHITELIST = (
-  'http://localhost:3000',
-)
+# CORS_ORIGIN_WHITELIST = (
+#   'http://localhost:3000',
+# )
 
 ROOT_URLCONF = 'gagarian_rest.urls'
 
