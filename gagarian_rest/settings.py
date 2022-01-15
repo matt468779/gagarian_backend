a176731314376d4890626fdc61a1e5bdf8af8b50
matt468779 <mengistusima@gmail.com>
2022-01-15 14:31:47 +0300

    database changed to postgresql

--- gagarian_rest/settings.py
+++ gagarian_rest/settings.py
@@ -116,8 +116,12 @@ WSGI_APPLICATION = 'gagarian_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'gagarian_db',
+        'USER': 'matt',
+        'PASSWORD': 'Password@123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
