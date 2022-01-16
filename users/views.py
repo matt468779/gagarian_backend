17a98b184a878a45773e2766580dcd63c9a49165
matt468779 <mengistusima@gmail.com>
2022-01-16 12:18:23 +0300

    minor fixes

--- users/views.py
+++ users/views.py
@@ -91,7 +91,7 @@ def addToCart(request):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 3
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
