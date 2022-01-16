f01285f255d5c6bfa96467b0d58f2a04844a0e9f
matt468779 <mengistusima@gmail.com>
2022-01-16 16:02:53 +0300

    minor fixes

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from users import views
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="index.html"), name='home'),
     path('users', views.all_users),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
@@ -15,6 +17,5 @@ urlpatterns = [
     path('checkout/', views.checkout, name="checkout"),
     path('packages/', views.packages, name='packages'),
     path('deleteAccount', views.deleteAccount, name='deleteAccount'),
-   # path('', views.home, name="home"),
 
 ]
--- users/views.py
+++ users/views.py
@@ -93,7 +93,7 @@ def addToCart(request):
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 10
+    page_size = 9
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
